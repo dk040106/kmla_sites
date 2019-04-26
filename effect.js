426fb54b0a7593b3e80bf438b2f4b712def72f25
dk040106 <48355099+dk040106@users.noreply.github.com>
2019-04-26 10:00:25 +0900

    목록을 접고 펼 수 있게 함

--- effect.js
+++ effect.js
@@ -1,6 +1,14 @@
-window.onload=function(){
+window.onload = function(){
   var con1 = document.getElementById("day");
   con1.style.display = 'none';
+  var divsToHide = document.getElementsByClassName("div1");
+  for(var i = 0; i < divsToHide.length; i++){
+      divsToHide[i].style.display = "none";
+  }
+  var divsToHide = document.getElementsByClassName("close");
+  for(var i = 0; i < divsToHide.length; i++){
+      divsToHide[i].style.display = "none";
+  }
 }
 function doDisplay1(){
     var con1 = document.getElementById("day");
@@ -22,10 +30,96 @@ function doDisplay2(){
 
   if(con2.style.display=='none'){
       con2.style.display = 'block';
-  }else{
+  }
+  else{
       con2.style.display = 'none';
       con1.style.display = 'block';
       document.body.style.backgroundColor = 'black';
       document.body.style.color = 'white';
+  }
+}
+
+function showNhideDis1() {
+  con1 = document.getElementById("dis1");
+  con2 = document.getElementById("open1");
+  con3 = document.getElementById("close1");
+
+  if(con1.style.display == 'block'){
+    con1.style.display = 'none';
+  }
+  else {
+    con1.style.display = 'block';
+  }
+
+  if(con2.style.display == 'none'){
+      con2.style.display = 'inline';
+      con3.style.display = 'none';
+  }
+  else{
+      con2.style.display = 'none';
+      con3.style.display = 'inline';
+  }
+}
+function showNhideDis2() {
+  con1 = document.getElementById("dis2");
+  con2 = document.getElementById("open2");
+  con3 = document.getElementById("close2");
+
+  if(con1.style.display == 'block'){
+    con1.style.display = 'none';
+  }
+  else {
+    con1.style.display = 'block';
+  }
+
+  if(con2.style.display == 'none'){
+      con2.style.display = 'inline';
+      con3.style.display = 'none';
+  }
+  else{
+      con2.style.display = 'none';
+      con3.style.display = 'inline';
+  }
+}
+function showNhideDis3() {
+  con1 = document.getElementById("dis3");
+  con2 = document.getElementById("open3");
+  con3 = document.getElementById("close3");
+
+  if(con1.style.display == 'block'){
+    con1.style.display = 'none';
+  }
+  else {
+    con1.style.display = 'block';
+  }
+
+  if(con2.style.display == 'none'){
+      con2.style.display = 'inline';
+      con3.style.display = 'none';
+  }
+  else{
+      con2.style.display = 'none';
+      con3.style.display = 'inline';
+  }
+}
+function showNhideDis4() {
+  con1 = document.getElementById("dis4");
+  con2 = document.getElementById("open4");
+  con3 = document.getElementById("close4");
+
+  if(con1.style.display == 'block'){
+    con1.style.display = 'none';
+  }
+  else {
+    con1.style.display = 'block';
+  }
+
+  if(con2.style.display == 'none'){
+      con2.style.display = 'inline';
+      con3.style.display = 'none';
+  }
+  else{
+      con2.style.display = 'none';
+      con3.style.display = 'inline';
   }
 }
